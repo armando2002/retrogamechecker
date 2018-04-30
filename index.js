d6fd12c20f45a0ae916afd9bc65c1821bfacd481
Stephen Williams <armando2002@gmail.com>
2018-04-29 19:17:40 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+
+//store necessary keys
+const TWITCH_CLIENT_ID = "cxzy4rz2gbrana3nh73ty2sgvj8s01";
+const VGPC_API_KEY = "d54ae2255c2fe8e39e936c398404eb52844da006";
+
+
+// add static example of pulling images first
+    
+// static example where user selects image and sees price (hoping there's a shared ID)
+
+});
+
+// need to think about way to pair price and image API requests if it is needed
+
+// think of a way to create a dynamic dropdown to filter console based on query, maybe using a GET request to populate an array based on console in the JSON response?
