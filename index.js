4f1c3bdeff15e5ace8f970887d317b6871f225db
Stephen Williams <armando2002@gmail.com>
2018-04-30 20:52:51 -0700

    initial commit

--- index.js
+++ index.js
@@ -5,14 +5,21 @@ $( document ).ready(function() {
     const VGPC_API_KEY = "d54ae2255c2fe8e39e936c398404eb52844da006";
     const GB_KEY = "4481200dc9e8799d83b4735342f8419d172c16cf";
 
-    var testFunction = function(arg1){
-        console.log(arg1);
 
+    // create a div element containing the game information
+    function renderResults(result) {
+        return `
+        <div>
+            <h2> ${result.products.product-name} </h2>
+        </div>`;
     }
-    
-    var testFunction2 = function(arg2){
-        console.log(arg2);
+
+    // function to add each HTML Div to page using .html
+    function displayResults(data) {
+        const results = data.products.map((item, index) => renderResults(item));
+        $('.js-main').html(results);
     }
+
     // call VGPC for prices
     $.ajax({
         url: 'https://www.pricecharting.com/api/products?',
@@ -21,18 +28,8 @@ $( document ).ready(function() {
             t: VGPC_API_KEY
         },
         // make a 2nd ajax call to twitch to grab images based on IDs (if they match)
-        success: testFunction,
+        success: displayResults,
         });
-
-    // call Twitch for image based on ID from VGPC
-    $.ajax({
-        url: 'https://api.twitch.tv/helix/games',
-        data: {
-            'Client-ID': TWITCH_CLIENT_ID,
-            id: '12139',
-        },
-        success: testFunction2,
-    });
 
     // add static example of pulling images first
         
@@ -45,4 +42,5 @@ $( document ).ready(function() {
         // need to convert price to USD (ex. 4009 to $40.09)
         // parseFloat(4009/100).toFixed(2) per Krishna
 
-        // think of a way to create a dynamic dropdown to filter console based on query, maybe using a GET request to populate an array based on console in the JSON response?
+        // think of a way to create a dynamic dropdown to filter console based on query,
+        // maybe using a GET request to populate an array based on console in the JSON response?
