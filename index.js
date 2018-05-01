c7b4e862a375a2492b179959349b7facbab06d0d
Stephen Williams <armando2002@gmail.com>
2018-05-01 09:42:34 -0700

    testing price and product name

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ $( document ).ready(function() {
     // create a div element containing the game information
     function renderResults(result) {
         console.log(result);
-        console.log(`${result.product-name}`);
+        console.log(result.product-name);
+        console.log(`${result.id}`);
+       // console.log(`${result.product-name}`);
     }
 
     // function to add each HTML Div to page using .html
